fb340b069bb35890dcfd3747e65589e6bb7114c5
Andrea.Accorinti <andrea.accorinti186@gmail.com>
2023-05-23 12:55:41 +0200

    1)Setting the project

---
+++ src/main/java/com/example/pocskykafka/PoCskyKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocskykafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoCskyKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoCskyKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pocskykafka/PoCskyKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocskykafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoCskyKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
